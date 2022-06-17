d15ded8e33cd666db6e67055a75c896e9469296d
Dhruv454000 <dhruvkunjadiya55@gmail.com>
2022-06-17 08:41:12 +0530

    parent b4139424a3955d9f2f132ce8d2e64075d8cb825b author Dhruv454000 <dhruvkunjadiya55@gmail.com> 1655435472 +0530 committer Dhruv454000 <dhruvkunjadiya55@gmail.com> 1655971043 +0530
    Added helper function and min hits condition

--- Trackers/centroid_tracker/centroid.py
+++ Trackers/centroid_tracker/centroid.py
@@ -14,99 +14,122 @@ class Centroid_tracker():
         self.max_age = 10
         self.min_hits = 3
         self.thres_distance = 20
-        # self.centroids = []
 
     def add_track(self, id, bbox, centroid):
-        """Create a centroid track object and adds to tracks list
         """
-
-        '''
-        - add centroid to track list
-        - id++
-        - add instance from class track
-        - if hits > min_hits add track
-        '''
-
-        track=centroid_track(id, bbox, centroid, 1, 0)
-        self.tracks.append(track)
-        self.nextID +=1
-
-    def delete_track(self,id):
-        """Deletes a centroid track object from tracks list if the max age is crossed.
-        """
-        del self.tracks[id]
-        self.nextID -= 1
-
-    def update(self, detections, id):
-        """Returns the active list of centroid track objects"""
+        Adding newly tracked objects to "tracks".
         
-        '''
-        - check if bbox is empty, if its empty, increase miss and check if miss> max_age if it is delete_track and return
-        - create centroid array, n calculate centroid of all tracks
-        - if nextid is 0, means no objects are tracking, then add_track
-        - else, find the min. distance of old centroid n new centroid
-            - check whether the centroid is matched to any row or col, if not then this means this object was lost earlier and now found, and reset it miss to 0
-            - check if input centroids is less than last time, so some objects are lost, and icrease their miss and check if it >max_age deregister it.
-                - if not, then we new objects, so register them
-        '''
+        Args
+        id : new id to be assigned 
+        bbox : bounding box of the object
+        centroid : centroid of the object
 
-        if(detections==[] and self.tracks!=[]):
-            print(id)
-            self.tracks[max(0, id-1)].miss=self.tracks[max(0, id-1)].miss+1
-            if(self.tracks[max(0, id-1)].miss>=self.max_age):
-                self.delete_track(max(0, id-1))
-            return self.tracks
+        Returns
+        None
+        """
+        
+        track = centroid_track(id, bbox, centroid, 1, 0)
+        self.tracks.append(track)
+        self.nextID += 1
+        print("added id ",track.id, "succesfully")
 
-        inputCentroids = np.zeros((max(1, len(detections)), 2), dtype="int")
+    def delete_track(self):
+        """
+        Deletes a centroid track object from "tracks" if the object is disappeared for more than "max_age".
+        
+        Args
+        None
 
-        for i in range(0,len(detections)):
-            cX = int((detections[i][2][0]+ detections[i][2][1]) / 2.0)
-            cY = int((detections[i][2][2] + detections[i][2][3]) / 2.0)
+        Returns 
+        None
+        """
+
+        for track in self.tracks:
+            if(track.miss > self.max_age):
+                print("deleted id ",track.id, "succesfully")
+                self.tracks.remove(track)
+
+    def update(self, detections):
+        """
+        Returns the active list of centroid track objects.
+
+        Args
+        detections : list of bbox of detected objects
+
+        Returns
+        tracks : list of objects of class Track
+        """
+
+        if(detections == []):
+            if(self.tracks == []):
+                return self.tracks
+            else:
+                # increase every objects miss by 1
+                for track in self.tracks:
+                    track.miss +=1
+                self.delete_track()
+                return self.tracks
+
+        inputCentroids = np.zeros((len(detections), 2), dtype="int")
+
+        for i in range(len(detections)):
+            cX = int((detections[i][0]+ detections[i][1]) / 2.0)
+            cY = int((detections[i][2] + detections[i][3]) / 2.0)
             inputCentroids[i] = (cX, cY)
-        objectId=[]
-        objectCentroid=[]
+        
+        objectId = []
+        objectCentroid = []
         if len(self.tracks) == 0:
             for i in range(0, len(inputCentroids)):
-                self.add_track(self.nextID, detections,inputCentroids[i])
-        else:
-           
-            for i in range(0,len(self.tracks)):
-                objectId.append(self.tracks[i].id)
-                objectCentroid.append(self.tracks[i].centroid)
+                self.add_track(self.nextID, detections[i], inputCentroids[i])
+        else:      
+            for track in self.tracks:
+                objectId.append(track.id)
+                objectCentroid.append(track.centroid) 
             D = dist.cdist(np.array(objectCentroid), inputCentroids)
-            rows = D.min(axis=1).argsort()
-            cols = D.argmin(axis=1)[rows]
-            usedRows = set()
-            usedCols = set()
-            # loop over the combination of the (row, column) index tuples
-            for (row, col) in zip(rows, cols):
-                # if we have already examined either the row or column value before, ignore it val
-                if row in usedRows or col in usedCols:
-                    continue
-                print("ROW: ", row, "Object ID: ", objectId)
-                oid = objectId[row]
-                print("%%%%%%%% ",oid)
-                print(len(self.tracks))
-                self.tracks[min(len(self.tracks), oid )].centroid = inputCentroids[col]
-                self.tracks[min(len(self.tracks), oid )].miss = 0
-                # indicate that we have examined each of the row and column indexes, respectively
-                usedRows.add(row)
-                usedCols.add(col)
-            unusedRows = set(range(0, D.shape[0])).difference(usedRows)
-            unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+            unusedRows,unusedCols,matched= self.find_match(objectCentroid,inputCentroids)
+            for i in matched:
+                self.tracks[i[0]].bbox = detections[i[1]]
+                self.tracks[i[0]].centroid = inputCentroids[i[1]]
+                self.tracks[i[0]].miss = 0
+                self.tracks[i[0]].hits += 1
             if D.shape[0] >= D.shape[1]:
             # loop over the unused row indexes
-                for row in unusedRows:
+                for row in unusedRows: 
                     # grab the object ID for the corresponding row index and increment the disappeared counter
-                    print("ROW: ", row, "Object ID: ", objectId)
-                    oid =objectId[row]
-                    print("%%%%%%%% ",oid)
-                    print(len(self.tracks))
-                    self.tracks[min(len(self.tracks), oid )].miss += 1
-                    # check to see if the number of consecutive frames the object has been marked "disappeared" for warrants deregistering the object
-                    if self.tracks[min(len(self.tracks), oid )].miss >= self.max_age:
-                        self.delete_track(oid)
+                    self.tracks[row].miss += 1
             else:
                 for col in unusedCols:
-                    self.add_track(id,detections,inputCentroids[col])
-        return self.tracks
+                    self.add_track(self.nextID, detections[col], inputCentroids[col])
+        self.delete_track()
+        result=[a for a in self.tracks if a.hits>=self.min_hits]
+        return result
+        
+    def find_match(self,objectCentroid,inputCentroids):
+        """
+        Returns list of matched objects , unmatched tracks and unmatched detections.  
+        
+        Args
+        objectCentroid : list of centroids of existing objects.
+        inputCentroids : list of centroids of current detections.
+
+        unusedcols : index of newly detected tracks
+        matched : list of index of matching objects 
+        """
+        D = dist.cdist(np.array(objectCentroid), inputCentroids)
+        rows = D.min(axis=1).argsort()
+        cols = D.argmin(axis=1)[rows]
+        usedRows = set()
+        usedCols = set()
+        matched =[]
+        # loop over the combination of the (row, column) index tuples
+        for (row, col) in zip(rows, cols):
+            # if we have already examined either the row or column value before, ignore it
+            if row in usedRows or col in usedCols:
+                continue
+            usedRows.add(row)
+            usedCols.add(col)
+            matched.append((row,col))
+        unusedRows = set(range(0, D.shape[0])).difference(usedRows)
+        unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+        return unusedRows,unusedCols,matched
--- main.py
+++ main.py
@@ -1,59 +1,57 @@
 import cv2
-import numpy as np
-from imutils.video import VideoStream
-import argparse
-import imutils
-import time
-
 from Trackers.centroid_tracker.centroid import Centroid_tracker
-# from Detectors.detector import YOLO
 from Detectors.YOLO import yolo
+
 if __name__ == "__main__":
+    
+    # load the video
+    vs = cv2.VideoCapture("/home/dhruv/Downloads/campus4-c0.avi")
+
+    cfg_file = "Detectors/YOLO/darknet/cfg/yolov4-tiny.cfg"
+    weight_file = "Detectors/YOLO/darknet/yolov4-tiny.weights"
+    namesfile = "Detectors/YOLO/darknet/data/coco.names"
+    datafile = "Detectors/YOLO/darknet/cfg/coco.data"
+    class_names="Detectors/YOLO/darknet/data/coco.names"
+
+    # Initiate object detector
+    dect = yolo.Yolo(cfg_file, weight_file,namesfile, datafile)
+    
+if __name__ == "__main__":
+    
     # Add the arg parser
     
-    cap = cv2.VideoCapture(0)                             # Local Stored video detection - Set input video
     # load the video
-    # vs = VideoStream(src="/home/dhruv/Downloads/traffic.mp4").start()
-    # time.sleep(2.0)
+    vs = cv2.VideoCapture("/home/ppspr/Videos/car.mp4")
+
+    cfg_file = "Detectors/YOLO/darknet/cfg/yolov4.cfg"
+    weight_file = "Detectors/YOLO/darknet/yolov4.weights"
+    namesfile = "Detectors/YOLO/darknet/data/coco.names"
+    datafile = "Detectors/YOLO/darknet/cfg/coco.data"
+    class_names="Detectors/YOLO/darknet/data/coco.names"
+
     # Initiate object detector
-    cfg_file = "/home/dhruv/Multi-Cam-ReID/Detectors/YOLO/darknet/cfg/yolov4.cfg"
-    weight_file = "/home/dhruv/Multi-Cam-ReID/Detectors/YOLO/darknet/yolov4.weights"
-    namesfile = "/home/dhruv/Multi-Cam-ReID/Detectors/YOLO/darknet/data/coco.names"
-    datafile = "/home/dhruv/Multi-Cam-ReID/Detectors/YOLO/darknet/cfg/coco.data"
-    m=""
-    class_names="/home/dhruv/Multi-Cam-ReID/Detectors/YOLO/darknet/data/coco.names"
-    colors=""
     dect = yolo.Yolo(cfg_file, weight_file,namesfile, datafile)
     
     # Initiate tracker object
     ot = Centroid_tracker()
-    (H, W) = (None, None)
-
+    
     # run the while loop
-    # while True:
-    #     # read the next frame from the video stream and resize it
-        
-    #     # Read frame
-    #     frame = vs.read()
-    #     frame = imutils.resize(frame, width=400)
-    #     # if the frame dimensions are None, grab them
-    #     # if W is None or H is None:
-    #     #     (H, W) = frame.shape[:2]
-
-    #     # run detection and get bbox
-    #     a=dect.detect(frame)
-    #     # run tracker update to get tracked tracks
-    #     # display the output
-    while(True):
-        ret, frame = cap.read()                                 # Capture frame and return true if frame present
-        # For Assertion Failed Error in OpenCV
-        if not ret:                                                  # Check if frame present otherwise he break the while loop
+    while True:
+        # Read frame 
+        ret, frame = vs.read()
+        if ret!=True:
             break
-        # dect.detect(frame_read)
-        detections,res = dect.detect(frame)
+
+        frame=cv2.resize(frame,(640,480))
+
+        # run detection and get bbox
+        detections = dect.detect(frame)
+        # run tracker update to get tracked tracks
+        track_list = ot.update(detections)
+        detections = []
+        for track in track_list:
+            detections.append((track.bbox, track.id))
+        res = dect.draw_box(detections, frame)
         cv2.imshow("result",res)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
-        # run tracker update to get tracked tracks
-        ot.update(detections, ot.nextID)
